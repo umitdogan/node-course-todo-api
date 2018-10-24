3c41367547639e60d2a7bc117be1bc88c51a551f
Umit Dogan <umdogan@gmail.com>
2018-10-24 16:18:33 +0300

    Add config files

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
 
 // Connection URL
-const url = 'mongodb://localhost:27017/TodoApp';
+//const url = 'mongodb://localhost:27017/TodoApp';
 const parametre = { useNewUrlParser: true };
 mongoose.Promise = global.Promise;
-mongoose.connect( process.env.MONGODB_URI || url, parametre);
+//mongoose.connect( process.env.MONGODB_URI || url, parametre);
+mongoose.connect( process.env.MONGODB_URI, parametre);
 
 module.exports = {
     mongoose
--- server/server.js
+++ server/server.js
@@ -1,3 +1,14 @@
+var env= process.env.NODE_ENV || 'development';
+console.log('*******', env);
+if (env === 'development') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+   
+} else if (env === 'test') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI ='mongodb://localhost:27017/TodoAppTest'; 
+}
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -7,7 +18,8 @@ var { mongoose } = require('./db/mongoose');
 var { User } = require('./models/user');
 var { Todo } = require('./models/todo');
 var app = express();
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 //console.log(process.env);
 
 var app = express();
