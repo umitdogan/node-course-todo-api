5854924f8cb3576830fe00b4cda0539214822e36
Umit Dogan <umdogan@gmail.com>
2018-10-30 17:12:31 +0300

    Add POST /user/login

--- server/server.js
+++ server/server.js
@@ -106,7 +106,6 @@ app.patch('/todos/:id', (req,res) =>{
 // POST /User
 app.post('/users', (req, res) => {
     //console.log (req.body);
-
     var body = _.pick(req.body,['email','password']);
     var user = new User(body);
     
@@ -121,6 +120,20 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate,(req,res)=>{
     res.send(req.user);    
+});
+
+app.post('/users/login', (req, res) => {
+
+    var body = _.pick(req.body,['email','password']);
+
+    User.findByCredentials(body.email, body.password).then((user) =>{
+        return user.generateAuthToken().then((token)=>{
+            res.header('x-auth',token).send(user);
+         });
+    }).catch((e)=>{
+        res.status(400).send();
+    });
+
 });
 
 app.listen(port, () => {
