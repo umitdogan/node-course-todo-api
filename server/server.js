237ee7a13302a093e218815c2afec10da642ec0f
Umit Dogan <umdogan@gmail.com>
2018-10-31 11:29:30 +0300

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -123,7 +123,6 @@ app.get('/users/me', authenticate,(req,res)=>{
 });
 
 app.post('/users/login', (req, res) => {
-
     var body = _.pick(req.body,['email','password']);
 
     User.findByCredentials(body.email, body.password).then((user) =>{
@@ -133,7 +132,14 @@ app.post('/users/login', (req, res) => {
     }).catch((e)=>{
         res.status(400).send();
     });
+});
 
+app.delete('/users/me/token', authenticate, (req,res)=>{
+    req.user.removeToken(req.token).then(()=>{
+        res.status(200).send();
+    }, ()=>{ 
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
